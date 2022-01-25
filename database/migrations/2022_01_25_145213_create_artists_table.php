95d382e0b8956f5e13b555fda1839dd7f41d98f2
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-01-25 16:12:47 +0100

    Add migration artists

---
+++ database/migrations/2022_01_25_145213_create_artists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('surname', 30)->nullable();
+            $table-> year('start_activity');
+            $table->string('origin_country');
+            $table->string('music_genre');
+            $table->string('label', 30)->nullable();
+            $table->tinyInteger('album_count')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
