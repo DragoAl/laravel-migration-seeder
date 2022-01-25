63864a49ca13590eb837d6942cca7c227b8fa904
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-01-25 16:45:41 +0100

    Add songs table

---
+++ database/migrations/2022_01_25_153831_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('artist_id');
+            $table->bigInteger('albums_id')->nullable();
+            $table->year('exit_date');
+            $table->text('text_song');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
